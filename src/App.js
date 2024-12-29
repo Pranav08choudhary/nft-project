3c903c8284adb17469f23863e4c48ab8302c6f93
pranav08choudhary <pranavsingh4040@gmail.com>
2024-12-30 03:51:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import detectEthereumProvider from '@metamask/detect-provider'
+import KryptoBird from './abis/KryptoBird.json'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+
+class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  // function to detect ethereum provider
+  async loadWeb3() {
+    const provider = await detectEthereumProvider()
+
+    if (provider) {
+      console.log('ethereum wallet is connected')
+      window.web3 = new Web3(provider)
+    } else {
+      console.log('No ethereum wallet detected')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    // load accounts
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // load Kryptobirdz contract
+    const networkID = await web3.eth.net.getId()
+    const networkData = await KryptoBird.networks[networkID]
+
+    if (networkData) {
+      const abi = KryptoBird.abi
+      const address = networkData.address
+      const contract = new web3.eth.Contract(abi, address)
+      this.setState({ contract })
+
+      // load totalSupply
+      const totalSupply = await contract.methods.totalSupply().call()
+      this.setState({ totalSupply })
+
+      // set up an array to keep track of tokens
+      for (let i = 1; i <= totalSupply; i++) {
+        const KryptoBird = await contract.methods.KryptoBirdz(i - 1).call()
+        this.setState({
+          kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird],
+        })
+      }
+    } else {
+      window.alert('Smart contract not deployed')
+    }
+  }
+
+  // setting up mint function
+  mint = (kryptoBird) => {
+    this.state.contract.methods
+      .mint(kryptoBird)
+      .send({ from: this.state.account })
+      .once('receipt', (receipt) => {
+        this.setState({
+          kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird],
+        })
+      })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      contract: null,
+      totalSupply: 0,
+      kryptoBirdz: [],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          {console.log(this.state.kryptoBirdz)}
+          <Navbar account={this.state.account} />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/mint"
+              element={
+                <Main mint={this.mint} kryptoBirdz={this.state.kryptoBirdz} />
+              }
+            />
+          <Route
+            exact path="/aboutus" element={<About />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export class Navbar extends Component {
+  render(props) {
+    return (
+      <div className='nav2'>
+        <nav className="navbar navbar-expand-lg " id='navbar'>
+          <div className="nav1 container-fluid ">
+            <p className="navbar-brand text-light" style={{margin: '5px'}} href="/">
+              <strong>NFT Marketplace</strong>
+            </p>
+            <ul className="navbar-nav me-auto mb-2 mx-3 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active text-light"
+                  aria-current="page"
+                  to="/"
+                >
+                  <strong>Home</strong>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active text-light"
+                  aria-current="page"
+                  to="/mint"
+                >
+                  <strong>Mint</strong>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active text-light"
+                  aria-current="page"
+                  to="/aboutus"
+                >
+                  <strong>About Us</strong>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <small className="text-light mx-3 px-2"><strong>{this.props.account}</strong></small>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
